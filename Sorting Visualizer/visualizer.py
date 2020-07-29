27ed6098d55462b19d9eed5819bf8331ecad2864
LCVcode <lcvcode@gmail.com>
2020-07-29 15:24:44 -0400

    Renamed pygame import to pg and removed unnecessary semicolons

--- Sorting Visualizer/visualizer.py
+++ Sorting Visualizer/visualizer.py
@@ -2,7 +2,7 @@ import algorithms
 import time
 import os
 import sys
-import pygame
+import pygame as pg
 
  # Set the window length and breadth  (Make sure that the breadth is equal to size of array. [512])
 dimensions = [1024, 512]
@@ -16,22 +16,22 @@ if len(sys.argv) > 1:
         print("")
         sys.exit(0)
 
-# Initalise the pygame library
-pygame.init()
+# Initalise the pg library
+pg.init()
 # Set the dimensions of the window and display it
-display = pygame.display.set_mode((dimensions[0], dimensions[1]))
+display = pg.display.set_mode((dimensions[0], dimensions[1]))
 # Fill the window with purple hue
-display.fill(pygame.Color("#a48be0"))
+display.fill(pg.Color("#a48be0"))
 
-def check_events(): # Check if the pygame window was quit
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit();
-            sys.exit();
+def check_events(): # Check if the pg window was quit
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
 
 def update(algorithm, swap1=None, swap2=None, display=display): # The function responsible for drawing the sorted array on each iteration
-    display.fill(pygame.Color("#a48be0"))
-    pygame.display.set_caption("Sorting Visualizer     Algorithm: {}     Time: {:.3f}      Status: Sorting...".format(algorithm.name, time.time() - algorithm.start_time)) # Display on title bar
+    display.fill(pg.Color("#a48be0"))
+    pg.display.set_caption("Sorting Visualizer     Algorithm: {}     Time: {:.3f}      Status: Sorting...".format(algorithm.name, time.time() - algorithm.start_time)) # Display on title bar
     k = int(dimensions[0]/len(algorithm.array))
     for i in range(len(algorithm.array)):
         colour = (80, 0, 255)
@@ -40,16 +40,16 @@ def update(algorithm, swap1=None, swap2=None, display=display): # The function r
         elif swap2 == algorithm.array[i]:
             colour = (255,0,0)
         # The most important step that renders the rectangles to the screen that gets sorted.
-        # pygame.draw.rect(dsiplay_window, color_of_rectangle, size_of_rectangle)
-        pygame.draw.rect(display, colour, (i*k,dimensions[1],k,-algorithm.array[i]))
+        # pg.draw.rect(dsiplay_window, color_of_rectangle, size_of_rectangle)
+        pg.draw.rect(display, colour, (i*k,dimensions[1],k,-algorithm.array[i]))
     check_events()
-    pygame.display.update()
+    pg.display.update()
 
 def keep_open(algorithm, display, time): # Keep the window open until sort completion
-    pygame.display.set_caption("Sorting Visualizer     Algorithm: {}     Time: {:.3f}      Status: Done!".format(algorithm.name, time))
+    pg.display.set_caption("Sorting Visualizer     Algorithm: {}     Time: {:.3f}      Status: Done!".format(algorithm.name, time))
     while True:
         check_events()
-        pygame.display.update()
+        pg.display.update()
 
 def main():
     if len(sys.argv) < 2:
